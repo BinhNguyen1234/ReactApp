ad1556a0e351306f062ed34fde14f3789e9ba169
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-12-18 22:16:59 +0700

    ok

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import SigninPage from './pages/singin'
 import SignUpPage from './pages/signup'
+import Home from './pages/home'
 export const router = createBrowserRouter([
     {
         path: '/signin',
@@ -9,5 +10,9 @@ export const router = createBrowserRouter([
     {
         path: '/signup',
         element: <SignUpPage></SignUpPage>
+    },
+    {
+        path: '*',
+        element: <Home></Home>
     }
 ])
