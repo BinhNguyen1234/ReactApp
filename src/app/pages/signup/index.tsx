e54a72d5d4ab33b9b2131d76768737b4af567193
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2024-12-30 22:29:34 +0700

    update package

--- src/app/pages/signup/index.tsx
+++ src/app/pages/signup/index.tsx
@@ -1,3 +1,5 @@
+import { JSX } from "react";
+
 export default function SignUpPage (): JSX.Element {
     return <div>Sign Up</div>
 }
--- src/app/pages/singin/index.tsx
+++ src/app/pages/singin/index.tsx
@@ -1,3 +1,5 @@
+import { JSX } from "react";
+
 export default function SigninPage (): JSX.Element {
     return <div>
         sign pages
