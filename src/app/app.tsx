6a6cf73f69ca5c9513500916e2e10aee4480328a
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2025-02-20 22:56:31 +0700

    Add global.css file to app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { router } from './routes'
-
+import "./global.css"
 
 export default function App(){
     return (
